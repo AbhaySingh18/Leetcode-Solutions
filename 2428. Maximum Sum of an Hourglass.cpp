33bfa2e31afc707e982a52d3f862bfac6bafe83c
Abhay Singh Rathore <109500977+AbhaySingh18@users.noreply.github.com>
2023-07-05 10:52:17 +0530

    2428. Maximum Sum of an Hourglass

---
+++ 2428. Maximum Sum of an Hourglass.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSum(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        int ans=0;
+        for(int i=0;i<n-2;i++){
+            for(int j=0;j<m-2;j++){
+                ans=max(ans,(grid[i][j]+grid[i][j+1]+grid[i][j+2]+grid[i+1][j+1]+grid[i+2][j]+grid[i+2][j+1]+grid[i+2][j+2]));
+            }
+        }
+        return ans;
+    }
+};
